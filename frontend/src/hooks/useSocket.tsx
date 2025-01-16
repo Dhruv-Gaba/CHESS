6d80091e92e9cd8238fac14094d1f5f254e7e6ba
DHRUV GABA <dhruv18gaba@gmail.com>
2025-01-17 01:03:27 +0530

    deployment changes

--- frontend/src/hooks/useSocket.tsx
+++ frontend/src/hooks/useSocket.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-const WS_URL="ws://localhost:8080";
+// const WS_URL="ws://localhost:8080";
+const WS_URL = "wss://chess-multiplayer-jlcs.onrender.com";
 
 export const useSocket=()=>{
     const [socket, setSocket] = useState<WebSocket | null>(null);
